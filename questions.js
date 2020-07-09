4f39b614f197e5d668a6ea5f7e457b79cc96c9ee
ianjameswilson <60559809+ianjameswilson@users.noreply.github.com>
2020-07-09 16:48:29 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+// questions *****************************************************
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+]
